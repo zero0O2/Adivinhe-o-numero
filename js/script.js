09dcd95e8a939f0f67a8e736003b05babf3a5ca2
zero0O2 <148266981+zero0O2@users.noreply.github.com>
2024-05-05 18:24:37 -0300

    termino

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let button = document.getElementById('button');
+let resultado = document.getElementById('resultado');
+let numeroAleatorio = Number.parseInt(Math.random() * 100)
+resultado.innerHTML = ''
+console.log(numeroAleatorio)
+
+button.addEventListener('click',function(){
+    let numeroJogador = Number(prompt('Digite seu palpite :'))
+    if(Number.isNaN(numeroJogador) == true){
+        alert('O jogo so aceita números')
+        
+    }else if (numeroJogador != numeroAleatorio){
+        if(numeroJogador < numeroAleatorio){resultado.innerHTML += `<p>Meu número é <strong>maior</strong> que ${numeroJogador} <br></p>`}
+        if(numeroJogador > numeroAleatorio){resultado.innerHTML += `<p>Meu número é <strong>menor</strong> que ${numeroJogador} <br></p>`}
+    }else {resultado.innerHTML += `<p>PARABÉNS,meu numero era <strong>${numeroJogador}</strong></p>`}
+});
