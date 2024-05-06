90ac3d0587b1d9e71c783291cbdd017df83caa14
zero0O2 <148266981+zero0O2@users.noreply.github.com>
2024-05-05 21:09:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 let button = document.getElementById('button');
 let resultado = document.getElementById('resultado');
-let numeroAleatorio = Number.parseInt(Math.random() * 1000000)
+let numeroAleatorio = Number.parseInt(Math.random() * 1000)
+let contagem = 0
 resultado.innerHTML = ''
 
 button.addEventListener('click',function(){
     let numeroJogador = Number(prompt('Digite seu palpite :'))
+    contagem += 1
     if(Number.isNaN(numeroJogador) == true){
         alert('O jogo so aceita números')
         
     }else if (numeroJogador != numeroAleatorio){
         if(numeroJogador < numeroAleatorio){resultado.innerHTML += `<p>Meu número é <strong>maior</strong> que ${numeroJogador} <br></p>`}
         if(numeroJogador > numeroAleatorio){resultado.innerHTML += `<p>Meu número é <strong>menor</strong> que ${numeroJogador} <br></p>`}
-    }else {resultado.innerHTML += `<p>PARABÉNS,meu numero era <strong>${numeroJogador}</strong></p>`}
+    }else {resultado.innerHTML += `<p>PARABÉNS,meu numero era <strong>${numeroJogador}</strong> seu numero tentativas foram ${contagem}</p>`}
 });
